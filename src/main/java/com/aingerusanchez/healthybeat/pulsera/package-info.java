64a571c8f8f06a94164b54f8ac139817398bf5e3
Aingeru Sanchez <Aingeru1703@gmail.com>
2018-04-24 23:10:08 +0200

    Initial commit

---
+++ src/main/java/com/aingerusanchez/healthybeat/pulsera/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Simulador de la pulsera con el pletismógrafo (servidor) que envia datos vía Bluetooth al dispositivo Android (cliente)
+ */
+/**
+ * @author Aingeru Sanchez
+ * 
+ */
+package com.aingerusanchez.healthybeat.pulsera;
